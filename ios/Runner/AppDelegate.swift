f1be2ec5ae55d48506dcff96b4abd879b9851801
yoshichibaaa <yoshi19941117@gmail.com>
2024-11-30 08:25:34 +0900

    iOS側の設定を加える

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,15 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("YOUR KEY HERE")
+    // Config.plist から API キーを読み込む
+    if let path = Bundle.main.path(forResource: "Config", ofType: "plist"),
+       let config = NSDictionary(contentsOfFile: path),
+       let apiKey = config["GoogleMapsAPIKey"] as? String {
+        GMSServices.provideAPIKey(apiKey)
+    } else {
+        fatalError("Google Maps API Key is missing in Config.plist")
+    }
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
